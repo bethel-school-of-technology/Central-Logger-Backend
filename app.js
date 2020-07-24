7cbe10005438bbbe8fb8500292919d4c01885c4f
Tate <tatefowble@gmail.com>
2020-07-24 12:34:26 -0500

    Version 1.0

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+let bodyparser = require('body-parser');
+let cors = require('cors');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+module.exports = app;
