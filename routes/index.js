eb46346359b9a92a0bfdc6e54e7f02401ab8a37e
TateFowble <tatefowble@gmail.com>
2020-08-15 22:48:42 -0500

    Added

--- routes/index.js
+++ routes/index.js
@@ -33,7 +33,10 @@ connection.query(query, (err, results) => {
 
 /* GET home page. */
 router.get('/',(req,res,next) => {
-    res.render('index');
+    //res.render('index');
+    res.json({
+        message: 'Request Completed Succesfully!'
+    })
 });
 
 module.exports = router;
